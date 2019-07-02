7c6ad9ee22cd1e3d5d464cda5b7d224f87b57603
Said abedi <abedi.designs@gmail.com>
2019-07-02 18:20:41 -0500

    Updates

---
+++ precache-manifest.f4cb63be8da3119b8f97b5effc93bd30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7adc885fc086ab4ca2930638aabdb750",
+    "url": "/highlow/index.html"
+  },
+  {
+    "revision": "0319b5ca33841b9dd0b5",
+    "url": "/highlow/static/css/main.3fd59052.chunk.css"
+  },
+  {
+    "revision": "7e7a274721e8c57a668b",
+    "url": "/highlow/static/js/2.0a38cd51.chunk.js"
+  },
+  {
+    "revision": "0319b5ca33841b9dd0b5",
+    "url": "/highlow/static/js/main.ab4ef498.chunk.js"
+  },
+  {
+    "revision": "97ae6af46518daab4265",
+    "url": "/highlow/static/js/runtime~main.ebe819fc.js"
+  },
+  {
+    "revision": "4c598d86e4daaf6774479f8e8c9b0f8c",
+    "url": "/highlow/static/media/casino-table.4c598d86.png"
+  }
+]);
